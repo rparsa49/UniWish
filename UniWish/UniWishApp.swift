d13a2dacb3cf7da680e39b375c99de9a7e82de71
roya parsa <99053997+rparsa49@users.noreply.github.com>
2023-09-04 06:45:21 -0400

    Initial Commit

---
+++ UniWish/UniWishApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniWishApp.swift
+//  UniWish
+//
+//  Created by Roya Parsa on 9/4/23.
+//
+
+import SwiftUI
+
+@main
+struct UniWishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
